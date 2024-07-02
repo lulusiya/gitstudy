e8c9bec51ae6a8d7780d6fc90a55f358186b62f5
lulusiya <504881442@qq.com>
2024-07-02 11:06:13 +0800

    第一次提交

---
+++ src/main/java/org/example/gutdemo/GutDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.gutdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GutDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GutDemoApplication.class, args);
+	}
+
+}
